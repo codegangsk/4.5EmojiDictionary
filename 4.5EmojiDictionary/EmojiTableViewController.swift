dde4c8a1d6e657a8ff4e17d61273acc6c41fe695
codegangsk <kjhj0604@gmail.com>
2020-09-08 23:24:25 +0900

    create EmojiTableViewController class  and assign it as table view controller's custom class

---
+++ 4.5EmojiDictionary/EmojiTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiTableViewController.swift
+//  4.5EmojiDictionary
+//
+//  Created by Sophie Kim on 2020/09/08.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
