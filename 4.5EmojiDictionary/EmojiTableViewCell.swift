3d53653b97df94304d56c5101b467d62fae03fd9
codegangsk <kjhj0604@gmail.com>
2020-09-11 09:48:24 +0900

    create tableViewCell file and add outlets for custom cell's labels

---
+++ 4.5EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  4.5EmojiDictionary
+//
+//  Created by Sophie Kim on 2020/09/11.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
